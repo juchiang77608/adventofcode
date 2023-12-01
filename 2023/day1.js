3e7c3d2e6c9193278c4ad8b731fe7efe1a98e3ab
Mien-Min Chiang <juchiang77608@gmail.com>
2023-12-02 01:21:51 +0800

    [feat]finish day1 Part 2

--- 2023/day1.js
+++ 2023/day1.js
@@ -1,3 +1,4 @@
+// Part 1
 // 題目：The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
 // 先將每列文字依照畫面斷行塞入換行符號\n字串陣列中
@@ -1013,6 +1014,7 @@ let day1 = document.querySelector('#day1');
 function arrange() {
   const array = [];
   let topic = rawTopic.split('\n');
+  topic = this.findLetters(topic);
   for (let index = 0; index < topic.length; index++) {
     let element = topic[index];
     element = element.replace(/[^0-9]/gi, '');
@@ -1030,12 +1032,46 @@ function arrange() {
  * @returns
  */
 function SumArray(arr) {
-  console.log('arr', arr);
+  // console.log('arr', arr);
   let sum = 0;
   for (let index = 0; index < arr.length; index++) {
     sum += arr[index];
   }
+  // console.log(sum);
   return sum;
 }
 day1.innerHTML = arrange();
 
+// Part 2
+// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+/**
+ * 將英文字母的數字文字轉成數字
+ * @param {*} topic
+ * @returns
+ */
+function findLetters(topic) {
+  const compare = {
+    two: '2',
+    one: '1',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+  for (let index = 0; index < topic.length; index++) {
+    // 陷阱 twone,eightwo
+    let letter = topic[index].replace('twone', 'twoone').replace('eightwo','eighttwo');
+    letter = Object.keys(compare).reduce(function (prev, key) {
+      prev = prev.replace(key, compare[key]);
+      // console.log(prev);
+      return prev;
+    }, letter);
+    topic[index] = letter;
+  }
+  // console.log(topic);
+  return topic;
+}
